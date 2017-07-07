cd3942ab4c33b273e5ceb9bda827ab1dd5f0812b
Pei Xiong Liu <pliu@fiksu.com>
2017-07-06 22:04:54 -0400

    Add validations to email input field

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,5 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  isDisabled: true
+  emailAddress: '',
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid')
 });
