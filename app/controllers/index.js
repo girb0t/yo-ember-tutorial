be374fefb3bc4a4acae2f1ea33c1a0fa320b9e60
Pei Xiong Liu <peixiong.liu@gmail.com>
2017-07-14 09:24:32 -0400

    Use promise when saving email

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -10,10 +10,11 @@ export default Ember.Controller.extend({
   actions: {
     saveInvitation() {
       const email = this.get('emailAddress');
-      const newInvitation = this.store.createRecord('invitation', { email: email });
-      newInvitation.save();
-      this.set('responseMessage', `Thank you! We have just saved your email address: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
+      const newInvitation = this.store.createRecord('invitation', { email });
+      newInvitation.save().then((response) => {
+        this.set('responseMessage', `Thank you! We have just saved your email address: ${response.get('id')}`);
+        this.set('emailAddress', '');
+      });
     }
   }
 });
